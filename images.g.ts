ab50990f476310f1da09103baefbc1cb8b46e2e4
Dr. Adam Smeets <90738+asmeets@users.noreply.github.com>
2026-04-21 22:04:31 -0500

    Update main.blocks, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -1,157 +1,33 @@
 // Auto-generated code. Do not edit.
 namespace myImages {
+
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "garageCone": return garageCone;
-            case "image2":
-            case "playerCar": return playerCar;
-            case "image3":
-            case "garageBg": return garageBg;
-            case "image4":
-            case "track": return track;
-            case "image5":
-            case "checkpointGate": return checkpointGate;
-            case "image6":
-            case "weatherIconRain": return weatherIconRain;
-            case "image7":
-            case "windArrow": return windArrow;
-            case "image8":
-            case "pitMarker": return pitMarker;
-            case "image9":
-            case "trackObstacle": return trackObstacle;
-            case "image10":
-            case "finishBg": return finishBg;
-            case "image11":
-            case "trackBg": return trackBg;
-            case "image12":
-            case "weatherBg": return weatherBg;
-            case "image13":
-            case "rainPuddle": return rainPuddle;
-            case "image14":
-            case "tireSoft": return tireSoft;
-            case "image15":
-            case "tireMedium": return tireMedium;
-            case "image16":
-            case "tireHard": return tireHard;
-            case "image17":
-            case "teamBadge": return teamBadge;
-            case "image18":
-            case "roleCardSoftware": return roleCardSoftware;
-            case "image19":
-            case "roleCardStrategist": return roleCardStrategist;
-            case "image20":
-            case "finishBanner": return finishBanner;
-            case "image21":
-            case "namePlate": return namePlate;
-            case "image22":
-            case "bodyGrip": return bodyGrip;
-            case "image23":
-            case "bodyBalanced": return bodyBalanced;
-            case "image24":
-            case "bodySpeed": return bodySpeed;
-            case "image25":
-            case "enginePower": return enginePower;
-            case "image26":
-            case "engineBalanced": return engineBalanced;
-            case "image27":
-            case "engineEfficient": return engineEfficient;
-            case "image28":
-            case "roleCardData": return roleCardData;
-            case "image29":
-            case "roleCardPerformance": return roleCardPerformance;
-            case "image30":
-            case "pitCrewTool": return pitCrewTool;
-            case "image31":
-            case "telemetryScreen": return telemetryScreen;
-            case "grass": return grass;
-            case "sand": return sand;
-            case "roadPlain": return roadPlain;
-            case "roadStraight": return roadStraight;
-            case "roadBend": return roadBend;
-            case "roadCross": return roadCross;
-            case "roadArrow": return roadArrow;
-            case "roadCheckpoint": return roadCheckpoint;
+
         }
         return null;
     })
 
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const garageCone = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const playerCar = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const garageBg = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const track = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const checkpointGate = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const weatherIconRain = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const windArrow = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const pitMarker = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const trackObstacle = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const finishBg = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const trackBg = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const weatherBg = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const rainPuddle = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const tireSoft = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const tireMedium = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const tireHard = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const teamBadge = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roleCardSoftware = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roleCardStrategist = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const finishBanner = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const namePlate = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const bodyGrip = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const bodyBalanced = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const bodySpeed = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const enginePower = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const engineBalanced = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const engineEfficient = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roleCardData = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roleCardPerformance = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const pitCrewTool = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const telemetryScreen = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const grass = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const sand = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roadPlain = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roadStraight = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roadBend = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roadCross = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roadArrow = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._spriteImage
-    export const roadCheckpoint = image.ofBuffer(hex``);
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
 }
+// Auto-generated code. Do not edit.
